eacc675628395f49fd18ad921f5115272681584b
George L <zeniff81android@gmail.com>
2022-06-24 00:57:02 -0400

    tile - add placeholders

--- src/components/tile/Tile.js
+++ src/components/tile/Tile.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import './tile.css'
 
-import dummyIcon from '../../resources/dummyicon.jpg'
-import DummyRating from '../../resources/DummyRating'
+import dummyIcon from '../../resources/dummy/dummyicon.jpg'
+import DummyRating from '../../resources/dummy/DummyRating'
 
-function Tile() {
+function Tile({title, subtitle, icon, background, color}) {
   return (
-    <div className="tile">
+    <div className="tile" style={{background, color}}>      
       <img src={dummyIcon} alt="dummy" className="tile__icon" />
-      <div className="tile__title">Lesson 1</div>
-      <div className="tile__subtitle">What's your name</div>
+      <div className="tile__title">{title}</div>
+      <div className="tile__subtitle">{subtitle}</div>
       <DummyRating />
     </div>
   )
---
+++ src/resources/dummy/dummyTileInfo.js
@@ -0,0 +1,28 @@
+const data = [
+  {
+    id: 0,
+    title: 'Lesson 1',
+    subtitle: 'What\' is your name',
+    icon: 'https://via.placeholder.com/150',
+    background: 'white',
+    color: 'black'
+  },
+  {
+    id: 1,
+    title: 'Lesson 2',
+    subtitle: 'Where are you from?',
+    icon: 'https://via.placeholder.com/150',
+    background: 'lightyellow',
+    color: '#000'
+  },
+  {
+    id: 2,
+    title: 'Lesson 3',
+    subtitle: 'I am sick',
+    icon: 'https://via.placeholder.com/150',
+    background: 'green',
+    color: 'white'
+  }
+]
+
+export default data
