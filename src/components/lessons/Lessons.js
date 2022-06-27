e508140b596fc32b9c93703877ab71dff101205b
George L <zeniff81android@gmail.com>
2022-06-27 00:21:28 -0400

    fix lesson content

--- src/components/lessons/Lessons.js
+++ src/components/lessons/Lessons.js
@@ -2,19 +2,19 @@ import React from 'react'
 import './lessons.css'
 import Tile from '../tile/Tile'
 import dummyTileInfo from '../../resources/dummy/dummyTileInfo'
-import LessonContent from '../lesson-content/LessonHeader'
+import LessonContent from '../lesson-content/LessonContent'
 
 function Lessons() {
   const [tiles, setTiles] = React.useState([])
   const [currentTileId, setCurrentTileId] = React.useState(null)
-
+  
   React.useEffect(() => {
     setTiles(dummyTileInfo)
   }, [])
 
   return (
     <div className="lessons">
-      <LessonContent lesson={tiles[currentTileId]}/>
+      {currentTileId !==null && <LessonContent lesson={tiles[currentTileId]}/>}
       {tiles.map(tile => (
         <div
           className='tile-container'
