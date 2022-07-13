e019dc3fa574578bbef9166a7fe3d49084a92bd7
George L <zeniff81android@gmail.com>
2022-07-13 17:30:02 -0400

    Working on flashcards

--- src/components/display-flashcards/DisplayFlashcards.js
+++ src/components/display-flashcards/DisplayFlashcards.js
@@ -1,10 +1,38 @@
 import React from 'react'
-import './displayflashcards.css'
+import classes from './displayflashcards.module.css'
+import dog from '../../resources/dummy/dog.jpg'
 
 const DisplayFlashcards = () => {
-  const [items, setItems] = React.useState([])
+  const [cards, setCards] = React.useState([
+    {
+      id: 0,
+      front: dog,
+      back: 'dog',
+      flipped: false,
+      background: '#ffb553',
+      color: 'black'
+    },
+  ])
   return (
-    <div className="display-flashcards"></div>
+    <div
+      className={classes.displayFlashcards}>
+      {cards.map(card => (
+        <div
+          className={classes.card}
+          key={card.id}
+          style={{ background: card.background, color: card.color }}>
+          <div
+            className={classes.cardFront}>
+            <img
+              src={card.front} alt="front img" />
+          </div>
+          <div
+            className={classes.cardBack}>
+            <p>{card.back}</p>
+          </div>
+        </div>
+      ))}
+    </div>
   )
 }
 
